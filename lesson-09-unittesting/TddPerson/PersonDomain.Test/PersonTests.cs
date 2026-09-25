f742e727a413589dc589518ca25e022f1e77dec6
connorfidelak <cfid2003@gmail.com>
2026-09-25 08:29:21 -0600

    added lesson-09 folder and created template for project

---
+++ lesson-09-unittesting/TddPerson/PersonDomain.Test/PersonTests.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PersonDomain;
+
+namespace PersonDomain.Tests
+{
+    public class PersonTests
+    {
+    }
+}
---
+++ lesson-09-unittesting/TddPerson/PersonDomain/Person.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDomain
+{
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
